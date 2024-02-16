7ec2a8dc6876057dee35a389c6dd8eeb1f6e0e0b
nobody cares <146330319+sileanhell@users.noreply.github.com>
2024-02-16 02:55:54 +0200

    examples

--- LL_01/alphabetPosition.js
+++ LL_01/alphabetPosition.js
@@ -1,12 +1,14 @@
 function alphabetPosition(inputString) {
-    const letters = 'abcdefghijklmnopqrstuvwxyz';
-    const result = [];
+  const letters = "abcdefghijklmnopqrstuvwxyz";
+  const result = [];
 
-    for (let i = 0; i < inputString.length; i++) {
-        const value = inputString[i].toLowerCase()
-        if (!letters.includes(value)) continue;
-        result.push(letters.indexOf(value) + 1)
-    }
+  for (let i = 0; i < inputString.length; i++) {
+    const value = inputString[i].toLowerCase();
+    if (!letters.includes(value)) continue;
+    result.push(letters.indexOf(value) + 1);
+  }
 
-    return result.join(" ");
+  return result.join(" ");
 }
+
+console.log(alphabetPosition("The sunset sets at twelve o' clock.")); // '20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11'
--- LL_01/findNextSquare.js
+++ LL_01/findNextSquare.js
@@ -1,6 +1,9 @@
 function findNextSquare(num) {
-    const square = Math.sqrt(num)
-    if (square % 1 !== 0) return -1;
-    let nextSquare = square + 1;
-    return nextSquare * nextSquare;
+  const square = Math.sqrt(num);
+  if (square % 1 !== 0) return -1;
+  let nextSquare = square + 1;
+  return nextSquare * nextSquare;
 }
+
+console.log(findNextSquare(121)); // 144
+console.log(findNextSquare(23)); // -1
